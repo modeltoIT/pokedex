78a8254242ec21af9776d9e2d623f471953ada58
Dmytro Ivasenko <dmtivasenko@gmail.com>
2025-10-05 21:48:16 +0400

    feat: improve help command

--- src/command_help.ts
+++ src/command_help.ts
@@ -1,11 +1,13 @@
 import { State } from './state.js'
 
 export async function commandHelp(state: State) {
-	console.log(`
-Welcome to the Pokedex!
-Usage:
+	const { commands } = state;
 
-help: Displays a help message
-exit: Exit the Pokedex');`)
+	let details = 'Welcome to the Pokedex!\nUsage:\n';
 
+	for (const value of Object.values(commands)) {
+		details += `\n${value.name}: ${value.description}`;
+	}
+
+	console.log(details);
 };
